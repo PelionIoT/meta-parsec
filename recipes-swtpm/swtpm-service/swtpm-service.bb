945e71ca794470fbfaca06214bc07114ca0452f2
petedyerarm <pete.dyer@pelion.com>
2021-08-20 11:05:50 +0100

    Debugging

--- recipes-swtpm/swtpm-service/swtpm-service.bb
+++ recipes-swtpm/swtpm-service/swtpm-service.bb
@@ -4,7 +4,7 @@ LICENSE="Apache-2.0"
 LIC_FILES_CHKSUM = "file://${WORKDIR}/LICENSE;md5=1dece7821bf3fd70fe1309eaa37d52a2"
 
 DEPENDS += "ibmswtpm2"
-RDEPENDS_${PN} += " ibmswtpm2 tpm2-tools libtss2 libtss2-tcti-mssim"
+RDEPENDS_${PN} += " ibmswtpm2 tpm2-tools libtss2 libtss2-tcti-mssim libtss2-tcti-device"
 require swtpm-service.inc
 
 
--- recipes-swtpm/swtpm-service/swtpm-service/swtpm.sh
+++ recipes-swtpm/swtpm-service/swtpm-service/swtpm.sh
@@ -4,6 +4,21 @@
 
 echo "SWTPM Startup"
 
-echo "SWTPM Disabled"
+cd /userdata/parsec
+
+if [ ! -f .HWTPM_Initialised ]; then
+    # No HWTPM_Initialised file found, so treat this as the first run
+    echo "No HWTPM_Initialised found"
+    set -x
+    /bin/tpm2_startup -c -T device:/dev/tpm0
+    set +x
+    date > ./.HWTPM_Initialised
+else
+    echo "HWTPM_Initialised found"
+    set -x
+    /bin/tpm2_startup -T device:/dev/tpm0
+    set +x
+fi
 
 echo "SWTPM Startup Complete"
+
