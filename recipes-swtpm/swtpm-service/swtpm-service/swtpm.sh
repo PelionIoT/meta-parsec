e3187ad51dc662787c2db1cdb9c93dddfa3ba757
petedyerarm <pete.dyer@pelion.com>
2021-09-06 13:32:52 +0100

    Stop calling tpm_startup on initial boot - possibly not requited

--- recipes-swtpm/swtpm-service/swtpm-service/swtpm.sh
+++ recipes-swtpm/swtpm-service/swtpm-service/swtpm.sh
@@ -10,7 +10,7 @@ if [ ! -f .HWTPM_Initialised ]; then
     # No HWTPM_Initialised file found, so treat this as the first run
     echo "No HWTPM_Initialised found"
     set -x
-    /bin/tpm2_startup -c -T device:/dev/tpm0
+    #/bin/tpm2_startup -c -T device:/dev/tpm0
     set +x
     date > ./.HWTPM_Initialised
 else
