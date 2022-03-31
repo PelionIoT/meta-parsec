8b13d7052118564e052e88fbb625f33319bcf85d
petedyerarm <pete.dyer@pelion.com>
2022-03-31 09:57:38 +0100

    Update parsec service to version 1.0.0
    Updated the bitbake files to use version 1.0.0 of Parsec
    
    Added recipes for the latest version of parsec-tool.
    
    Update README.md to have a section about how to change to a newer version
    of parsec.
    
    Updated the initialisation phase to work with a read-only file system.  This
    is needed for SoftHSM configurations where the config.toml file gets updated
    during initialisation. This gets written to /userdata/parsec instead of being
    updated in place in /etc/parsec.

--- recipes-parsec/parsec-service/parsec-service/init_pkcs11_slots.sh
+++ recipes-parsec/parsec-service/parsec-service/init_pkcs11_slots.sh
@@ -19,16 +19,19 @@ PIN=87654321
 num_slots=$(pkcs11-tool --module $MODULE --list-slots | grep -c "^Slot")
 num_unintialized_slots=$(pkcs11-tool --module $MODULE --list-slots | grep -c "uninitialized")
 
-if [ $num_slots -gt 0 ]; then
-    if [ $num_unintialized_slots -eq $num_slots ]; then
+if [ "$num_slots" -gt 0 ]; then
+    if [ "$num_unintialized_slots" -eq "$num_slots" ]; then
         pkcs11-tool --module $MODULE --init-token --label "parsec" --so-pin ${SO_PIN}
         pkcs11-tool --module $MODULE --init-pin --so-pin ${SO_PIN} --pin ${PIN}
-        if [ "$MODULE" = /usr/lib/softhsm/libsofthsm2.so ]; then
-            # For the softhsm, the slot number is allocated at random during the init.
-            # Update the parsec config file with the new value.
-            slot=$(softhsm2-util --show-slots |   grep -m 1 ^Slot | cut -d" " -f2)
-            sed -i -e "s,slot_number = [0-9]*,slot_number = $slot,g" /etc/parsec/config.toml
-        fi
+    fi
+fi
+
+if [ "$MODULE" = /usr/lib/softhsm/libsofthsm2.so ]; then
+    if [ ! -f /userdata/parsec/config.toml ]; then
+        # For the softhsm, the slot number is allocated at random during the init.
+        # Update the parsec config file with the new value.
+        slot=$(softhsm2-util --show-slots |   grep -m 1 ^Slot | cut -d" " -f2)
+        sed -e "s,slot_number = [0-9]*,slot_number = $slot,g" /etc/parsec/config.toml > /userdata/parsec/config.toml
     fi
 fi
 
